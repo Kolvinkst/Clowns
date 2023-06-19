e2578b2d6e87a8fb3dfb5ca19a5a06f536020a22
Kolvinkst <einbenutzer1998@gmail.com>
2023-06-19 07:06:44 +0700

    Добавьте файлы проекта.

---
+++ Clown.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowns
+{
+    internal class Clown
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine("My name is " + Name +
+                " and I'm " + Height + " inches tall.");
+        }
+    }
+}
